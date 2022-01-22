4f02445e7b3087ab5216f56e534f0defccb5c893
Alex <sasha_lazur@yahoo.com>
2022-01-22 12:45:24 +0200

    cherry1

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,7 +5,10 @@ public class Main
 
     public static void main(String[] args)
     {
-
+        String s= "st0";
+        String s1 = "st01";
+        String s2= "st02";
+        String s3= "st03";
     }
 
 }
