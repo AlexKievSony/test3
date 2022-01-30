b8bc399056a06ef5eff84939f43f0ee81c1090e8
Alex <sasha_lazur@yahoo.com>
2022-01-30 15:30:46 +0200

    Branches2

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,14 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
-TestMethod <String,String>test= new TestMethod<String,String>();
-test.set("value");
 
-String s= "st";
-Object o= s;
-        Boolean a=true;
-Boolean t=a ? true : false;
-System.out.println(t);
     }
 
 }
