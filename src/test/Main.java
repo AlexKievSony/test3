fddafd5c19af59eeaf7a70bcd66f2b4f1f19156f
Alex <sasha_lazur@yahoo.com>
2022-03-21 15:56:02 +0200

    TestRevert

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,7 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        String s= "st0";
+
         String s1 = "st01";
         String s2= "st02";
         String s3= "st03";
